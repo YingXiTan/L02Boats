46cb0e862f4e847735001092966587f83cc97a8f
Shimo <123053703+YingXiTan@users.noreply.github.com>
2024-11-08 15:47:58 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Text, ScrollView, StyleSheet} from "react-native"; //Import to use <View>, <Text>, <Image>
+import Boat from "./Components/Boats.js";
 
-export default function App() {
+
+const header = StyleSheet.create({
+    fontWeight: "bold",
+    fontSize: 40,
+    marginTop: 20,
+})
+
+const AllBoats= () =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView scrollEventThrottle={1}>
+        <Text style={header}> Boats For Sale</Text>
+        <Boat
+            icon_name="sailboat" name="Sea Ray 500 Sundancer"
+            description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
+            picture={require('./img/sea_ray.jpg')}/>
+
+        <Boat
+            icon_name="sailboat" name="Four Winns Horizon 180"
+            description="A sporty look and refined details truly set the Horizon 180 above all others."
+            picture={require('./img/four_winns.jpg')}/>
+
+        <Boat
+            icon_name="sailboat" name="Flipper 640 ST"
+            description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
+            picture={require('./img/flipper.jpg')}/>
+
+        <Boat
+            icon_name="sailboat" name="Princess V48"
+            description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
+            picture={require('./img/princess.jpg')}/>
+
+        <Boat
+            icon_name="sailboat" name="Fairline Targa 47"
+            description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
+            picture={require('./img/fairline.jpg')}/>
+
+        <Boat
+            icon_name="sailboat" name="Bayliner 175 Bowrider"
+            description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
+            picture={require('./img/bayliner.jpg')}/>
+      </ScrollView>
   );
 }
+export default AllBoats;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- Components/Boats.js
+++ Components/Boats.js
@@ -1,21 +1,63 @@
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, StyleSheet, Button} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome6"
+import {Colors} from "react-native/Libraries/NewAppScreen";
+
+
+const styles = StyleSheet.create({
+    background: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderColor: 'black',
+        margin: 10,
+        marginTop: 20,
+        backgroundColor: '#EFF5F3',
+    },
+
+    title: {
+        flexDirection: 'row',
+        flex: 1
+    },
+
+    icon: {
+        size: 50,
+        color: 'black',
+    },
+
+    boatName: {
+        fontWeight: 'bold',
+        color: Colors.black,
+        fontSize: 20,
+    },
+
+    description: {
+        fontSize: 15,
+        marginLeft: 10,
+    },
+
+    image: {
+        width: 300,
+        height: 200,
+        marginBottom: 15
+    }
+})
 
 const AllBoats = ({name, description, icon_name, picture}) => {
     return (
-        <View>
-            <View style={{flexDirection: "row", alignItems: "center"}}>
-                <Icon name={icon_name} size={30} color={"grey"} style={{marginLeft: 10}}/>
-                <Text style={{fontSize: 20, fontWeight: "bold", marginLeft: 10}}>
-                    {name}
-                </Text>
-            </View>
+            <View style={styles.background}>
+                <View style={styles.title}>
+                    <Icon name={icon_name} style={styles.icon} />
+                    <Text style={styles.boatName}>
+                        {name}
+                    </Text>
+                </View>
 
-            <Text style={{fontSize: 14, marginLeft: 10}}>
-                {description}
-            </Text>
-            <Image source={picture} style={{width: 300, height: 200, marginLeft: 10}} />
-        </View>
-    );
+                <Text style={styles.description}>
+                    {description}
+                </Text>
+
+                <Image source={picture} style={styles.image} />
+            </View>
+    )
 };
 export default AllBoats;
